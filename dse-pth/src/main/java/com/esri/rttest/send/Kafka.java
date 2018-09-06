cdfb3d6117b31d0b8f5c8612c61d2a2325716065
Ricardo Trujillo <rtrujillo@esri.com>
2018-09-05 17:48:57 -0700

    Updates to dse-pth. Made it a docker container we can run via metronome.

--- dse-pth/src/main/java/com/esri/rttest/send/Kafka.java
+++ dse-pth/src/main/java/com/esri/rttest/send/Kafka.java
@@ -25,12 +25,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Properties;
 import java.util.UUID;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 
 /*
  * Sends lines of a text file to a Kafka Topic 
@@ -38,228 +38,248 @@ import org.apache.logging.log4j.Logger;
  * 
  * @author david
  */
-public class Kafka {
+public class Kafka
+{
 
-    private static final Logger LOG = LogManager.getLogger(Kafka.class);
-    
-    private Producer<String, String> producer;
-    private String topic;
-    
-    public Kafka(String brokers, String topic) {
-        
-        
+  private static final Logger LOG = LogManager.getLogger(Kafka.class);
 
-            // https://kafka.apache.org/documentation/#producerconfigs
-            Properties props = new Properties();
-            props.put("bootstrap.servers",brokers);
-            props.put("client.id", Kafka.class.getName());
-            props.put("acks", "1");
-            props.put("retries", 0);
-            props.put("batch.size", 16384);
-            props.put("linger.ms", 1);
-            props.put("buffer.memory", 8192000);
-            props.put("request.timeout.ms", "11000");
-            props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-            props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+  private Producer<String, String> producer;
+  private String                   topic;
+
+  public Kafka(String brokers, String topic)
+  {
+
+    // https://kafka.apache.org/documentation/#producerconfigs
+    Properties props = new Properties();
+    props.put("bootstrap.servers", brokers);
+    props.put("client.id", Kafka.class.getName());
+    props.put("acks", "1");
+    props.put("retries", 0);
+    props.put("batch.size", 16384);
+    props.put("linger.ms", 1);
+    props.put("buffer.memory", 8192000);
+    props.put("request.timeout.ms", "11000");
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
             /* Addin Simple Partioner didn't help */
-            //props.put("partitioner.class", SimplePartitioner.class.getCanonicalName());
-            
-            this.producer = new KafkaProducer<>(props);
-            this.topic = topic;
-            
-    }
-    
-    /**
-     * 
-     * @param filename File with lines of data to be sent.
-     * @param rate Rate in lines per second to send.
-     * @param numToSend Number of lines to send. If more than number of lines in file will resend from start.
-     * @param burstDelay Number of milliseconds to burst at; set to 0 to send one line at a time
-     */
-    public void sendFile(String filename, Integer rate, Integer numToSend, Integer burstDelay) {
-        try {
-            FileReader fr = new FileReader(filename);
-            BufferedReader br = new BufferedReader(fr);
-            
-            // Read the file into an array
-            ArrayList<String> lines = new ArrayList<>();
-            
-            
-            String line;
-            while ((line = br.readLine()) != null) {
-                lines.add(line);                
-            }
-            
-            br.close();
-            fr.close();
-            
-            Iterator<String> linesIt = lines.iterator();
-            
-            // Get the System Time
-            Long st = System.currentTimeMillis();
+    //props.put("partitioner.class", SimplePartitioner.class.getCanonicalName());
 
-            Integer cnt = 0;
-            
-            // Tweak used to adjust delays to try and get requested rate
-            Long tweak = 0L;              
+    this.producer = new KafkaProducer<>(props);
+    this.topic = topic;
+
+  }
+
+  /**
+   * @param filename   File with lines of data to be sent.
+   * @param rate       Rate in lines per second to send.
+   * @param numToSend  Number of lines to send. If more than number of lines in file will resend from start.
+   * @param burstDelay Number of milliseconds to burst at; set to 0 to send one line at a time
+   */
+  public void sendFile(String filename, Integer rate, Integer numToSend, Integer burstDelay)
+  {
+    try
+    {
+      FileReader fr = new FileReader(filename);
+      BufferedReader br = new BufferedReader(fr);
+
+      // Read the file into an array
+      ArrayList<String> lines = new ArrayList<>();
+
+      String line;
+      while ((line = br.readLine()) != null)
+      {
+        lines.add(line);
+      }
+
+      br.close();
+      fr.close();
+
+      Iterator<String> linesIt = lines.iterator();
+
+      // Get the System Time
+      Long st = System.currentTimeMillis();
+
+      Integer cnt = 0;
+
+      // Tweak used to adjust delays to try and get requested rate
+      Long tweak = 0L;
             
             /*
                 For rates < 100/s burst is better
                 For rates > 100/s continous is better            
             */
-            
-            
-            // *********** SEND Constant Rate using nanosecond delay *********
-            if (burstDelay == 0) {
-                // Delay between each send in nano seconds            
-                Double ns_delay = 1000000000.0 / (double) rate;
-                
-                // By adding some to the delay you can fine tune to achieve the desired output
-                ns_delay = ns_delay - (tweak * 100);
-                
-                long ns = ns_delay.longValue();
-                if (ns < 0) ns = 0;  // can't be less than 0 
 
+      // *********** SEND Constant Rate using nanosecond delay *********
+      if (burstDelay == 0)
+      {
+        // Delay between each send in nano seconds
+        Double ns_delay = 1000000000.0 / (double) rate;
 
+        // By adding some to the delay you can fine tune to achieve the desired output
+        ns_delay = ns_delay - (tweak * 100);
 
+        long ns = ns_delay.longValue();
+        if (ns < 0)
+          ns = 0;  // can't be less than 0
 
-                while (cnt < numToSend) {
-                    
-                   if (cnt % rate == 0 && cnt > 0) {
-                        // Calculate rate and adjust as needed
-                        Double curRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
+        while (cnt < numToSend)
+        {
 
-                        System.out.println(cnt + "," + String.format("%.0f", curRate));
-                    }
+          if (cnt % rate == 0 && cnt > 0)
+          {
+            // Calculate rate and adjust as needed
+            Double curRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
 
-                    if (cnt % 1000 == 0 && cnt > 0) {
-                        // Calculate rate and adjust as needed
-                        Double curRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
+            System.out.println(cnt + "," + String.format("%.0f", curRate));
+          }
 
-                        // rate difference as percentage 
-                        Double rateDiff = (rate - curRate) / rate;
+          if (cnt % 1000 == 0 && cnt > 0)
+          {
+            // Calculate rate and adjust as needed
+            Double curRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
 
-                        // Add or subracts up to 100ns 
-                        tweak = (long) (rateDiff * rate);
+            // rate difference as percentage
+            Double rateDiff = (rate - curRate) / rate;
 
-                        // By adding some to the delay you can fine tune to achieve the desired output
-                        ns = ns - tweak;
-                        if (ns < 0) {
-                            ns = 0;  // can't be less than 0 
-                        }
+            // Add or subracts up to 100ns
+            tweak = (long) (rateDiff * rate);
 
-                    }                             
-                    
-                    cnt += 1;
-
-                    if (!linesIt.hasNext()) linesIt = lines.iterator();  // Reset Iterator
-
-                    line = linesIt.next() + "\n";
-
-                    final long stime = System.nanoTime();
-                    
-                    UUID uuid = UUID.randomUUID();
-                    producer.send(new ProducerRecord<>(this.topic, uuid.toString(),line));                                        
-
-                    
-                    long etime;
-                    do {
-                        // This approach uses a lot of CPU                    
-                        etime = System.nanoTime();
-                        // Adding the following sleep for a few microsecond reduces the load
-                        // However, it also effects the through put
-                        //Thread.sleep(0,100);  
-                    } while (stime + ns >= etime);                
-
-                }
-            } else {
-                // *********** SEND in bursts every msDelay ms  *********
-
-                Integer msDelay = burstDelay;
-                Integer numPerBurst = Math.round(rate / 1000 * msDelay); 
-
-                if (numPerBurst < 1) numPerBurst = 1;
-                
-                Integer delay = burstDelay;
-
-                while (cnt < numToSend) {
-                    
-                   // Adjust delay every burst
-                    Double curRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
-                    Double rateDiff = (rate - curRate) / rate;
-                    tweak = (long) (rateDiff * rate);
-                    delay = delay - Math.round(tweak / 1000.0f);
-                    if (delay < 0) {
-                        delay = 0;  // delay cannot be negative
-                    } else {
-                        Thread.sleep(delay);
-                    }                    
-                                        
-                                        
-
-                    Integer i = 0;
-                    while (i < numPerBurst) {
-                        if (cnt % rate == 0 && cnt > 0) {
-                            curRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
-                            System.out.println(cnt + "," + String.format("%.0f", curRate));
-                        }                        
-                        
-                        cnt += 1;
-                        
-                        i += 1;
-                        if (!linesIt.hasNext()) linesIt = lines.iterator();  // Reset Iterator
-
-                        line = linesIt.next() + "\n";
-
-                        UUID uuid = UUID.randomUUID();
-                        producer.send(new ProducerRecord<>(this.topic, uuid.toString(),line));
-                        
-                        // Break out as soon as numToSend is reached
-                        if (cnt >= numToSend) {
-                            break;
-                        }                           
-                        
-                    }
-
-                }
-            
-            }            
-            
-            // This command was needed when running multiple instances of Kafka; otherwise, lines were lost
-            producer.flush();
-            
-            Double sendRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
-            
-            System.out.println(cnt + "," + String.format("%.0f", sendRate));
-                             
-        } catch (IOException | InterruptedException e) {
-            // Could fail on very large files that would fill heap space 
-            
-            LOG.error("ERROR", e);
-            
-        }
-    }
-    
-    
-    
-    public static void main(String args[]) throws Exception {
-        
-        // Command Line d1.trinity.dev:9092 simFile simFile_1000_10s.dat 1000 10000
-        
-        if (args.length != 5 && args.length != 6) {
-            System.err.print("Usage: Kafka <broker-list-or-hub-name> <topic> <file> <rate> <numrecords> (<burst-delay-ms>)\n");
-        } else {
-            
-            String brokers = args[0];
-            Kafka t = new Kafka(brokers, args[1]);
-            if (args.length == 5) {
-                t.sendFile(args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]), 0);
-            } else {
-                t.sendFile(args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]));
+            // By adding some to the delay you can fine tune to achieve the desired output
+            ns = ns - tweak;
+            if (ns < 0)
+            {
+              ns = 0;  // can't be less than 0
             }
 
+          }
+
+          cnt += 1;
+
+          if (!linesIt.hasNext())
+            linesIt = lines.iterator();  // Reset Iterator
+
+          line = linesIt.next() + "\n";
+
+          final long stime = System.nanoTime();
+
+          UUID uuid = UUID.randomUUID();
+          producer.send(new ProducerRecord<>(this.topic, uuid.toString(), line));
+
+          long etime;
+          do
+          {
+            // This approach uses a lot of CPU
+            etime = System.nanoTime();
+            // Adding the following sleep for a few microsecond reduces the load
+            // However, it also effects the through put
+            //Thread.sleep(0,100);
+          }
+          while (stime + ns >= etime);
+
         }
-                
-       
+      }
+      else
+      {
+        // *********** SEND in bursts every msDelay ms  *********
+
+        Integer msDelay = burstDelay;
+        Integer numPerBurst = Math.round(rate / 1000 * msDelay);
+
+        if (numPerBurst < 1)
+          numPerBurst = 1;
+
+        Integer delay = burstDelay;
+
+        while (cnt < numToSend)
+        {
+
+          // Adjust delay every burst
+          Double curRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
+          Double rateDiff = (rate - curRate) / rate;
+          tweak = (long) (rateDiff * rate);
+          delay = delay - Math.round(tweak / 1000.0f);
+          if (delay < 0)
+          {
+            delay = 0;  // delay cannot be negative
+          }
+          else
+          {
+            Thread.sleep(delay);
+          }
+
+          Integer i = 0;
+          while (i < numPerBurst)
+          {
+            if (cnt % rate == 0 && cnt > 0)
+            {
+              curRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
+              System.out.println(cnt + "," + String.format("%.0f", curRate));
+            }
+
+            cnt += 1;
+
+            i += 1;
+            if (!linesIt.hasNext())
+              linesIt = lines.iterator();  // Reset Iterator
+
+            line = linesIt.next() + "\n";
+
+            UUID uuid = UUID.randomUUID();
+            producer.send(new ProducerRecord<>(this.topic, uuid.toString(), line));
+
+            // Break out as soon as numToSend is reached
+            if (cnt >= numToSend)
+            {
+              break;
+            }
+
+          }
+
+        }
+
+      }
+
+      // This command was needed when running multiple instances of Kafka; otherwise, lines were lost
+      producer.flush();
+
+      Double sendRate = (double) cnt / (System.currentTimeMillis() - st) * 1000;
+
+      System.out.println(cnt + "," + String.format("%.0f", sendRate));
+
     }
+    catch (IOException | InterruptedException e)
+    {
+      // Could fail on very large files that would fill heap space
+
+      LOG.error("ERROR", e);
+
+    }
+  }
+
+  public static void main(String args[]) throws Exception
+  {
+
+    // Command Line d1.trinity.dev:9092 simFile simFile_1000_10s.dat 1000 10000
+
+    if (args.length != 5 && args.length != 6)
+    {
+      System.err.print("Usage: Kafka <broker-list-or-hub-name> <topic> <file> <rate> <numrecords> (<burst-delay-ms>)\n");
+    }
+    else
+    {
+
+      String brokers = args[0];
+      Kafka t = new Kafka(brokers, args[1]);
+      if (args.length == 5)
+      {
+        t.sendFile(args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]), 0);
+      }
+      else
+      {
+        t.sendFile(args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]));
+      }
+
+    }
+
+  }
 }
